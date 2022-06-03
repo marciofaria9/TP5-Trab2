4cfbd07ed7ccc20676c9ec92faef5cd113e59588
marciofaria9 <m.faria.jr@icloud.com>
2022-06-03 18:48:46 -0300

    adicionando arquivos do Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  timestamp: '1653661266',
+  publicKey: 'c83f0ed3e2c41fc8b24bbdc02b858a98' ,
+  hash: '604dbfb0d2240217af936477af75503d',
+
+  // URLs para acessar 
+  
+  URLCaracter: 'https://gateway.marvel.com/v1/public/characters?ts=',
+  URLComics:  'https://gateway.marvel.com/v1/public/comics?ts=',
+  URLEvents:  'https://gateway.marvel.com/v1/public/events?ts=',
+  URLSeries:  'https://gateway.marvel.com/v1/public/series?ts=',
+  URLStories:  'https://gateway.marvel.com/v1/public/stories?ts='
 };
